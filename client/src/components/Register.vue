dc9e3286d9d33b7cb0493275f984e47b9ce9608d
Jack Lee <jack@hotmail.com>
2018-02-23 12:59:40 -0500

    the client and server were set up

---
+++ client/src/components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input 
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email">
+    <br>
+    <input 
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password">
+    <br>
+    <button @click="register">Register</button>
+  </div>
+  
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email:'',
+      password:''
+    }
+  },
+  // watch:{
+  //   email(value){
+  //     console.log(`email has changed ${value}`);
+  //   }
+  // },
+
+  methods: {
+   async register(){
+      const response = await AuthenticationService.register({
+          email:this.email,
+           password: this.password
+       })
+      console.log(response.data)
+    }
+  },
+
+  // mounted(){
+  //   setTimeout(function(){
+  //     this.email = 'hello world'
+  //   },2000)
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
