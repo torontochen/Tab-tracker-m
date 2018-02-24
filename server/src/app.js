4cb24451f24ce646bdf87d345bb4120668b16405
Jack Lee <jack@hotmail.com>
2018-02-24 17:09:09 -0500

    Joi is set up

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-
-
+const {sequelize} = require('./models')
+const config = require('./config/config')
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/register',(req,res)=>{
-  res.send({
-   message:  `Hello ${req.body.email} Your user was registered!`
-  });
-});
-app.listen(process.env.PORT || 8081);
+require('./routes')(app)
+
+sequelize.sync({force:true})
+    .then(()=>{
+      app.listen(config.port)
+      console.log(`Server started on port ${config.port}`)
+    })
+
